f64708204e71e95d2fe86d7a786cd0acbbaccb7a
RiTiKoo3 <deathrode07@gmail.com>
2026-06-06 10:56:06 +0530

    server updated

--- server/app.js
+++ server/app.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const connectDB = require('./src/config/db')
+
+
+// Connect to MongoDB
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
