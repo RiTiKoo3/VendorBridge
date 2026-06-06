fbb4385a7cf30d4163c3cea4da44049132df77fb
RiTiKoo3 <deathrode07@gmail.com>
2026-06-06 13:37:27 +0530

    Server Phase 2 Completed

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const vendorRoutes = require('./routes/vendorRoutes');
 const rfqRoutes = require('./routes/rfqRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
+app.use('/api/auth', authRoutes)
 app.use('/api/vendors', vendorRoutes)
 app.use('/api/rfqs', rfqRoutes)
 
